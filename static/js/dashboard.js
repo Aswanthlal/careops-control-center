4c93c2fe91999f7226fe1675985e70bfc170144d
Aswanthlal <aswanthlal7.lbscek@.ac.in>
2026-02-13 16:57:58 +0530

    Fix_2

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,8 +1,7 @@
-fetch("/api/dashboard/")
+fetch(window.location.origin + "/api/dashboard/?t=" + new Date().getTime())
   .then(res => res.json())
   .then(data => {
     document.getElementById("todayBookings").innerText = data.today_bookings;
     document.getElementById("unansweredCount").innerText = data.unanswered_conversations;
     document.getElementById("lowStockCount").innerText = data.low_stock_alerts;
-    
   });
